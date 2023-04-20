a561f27d7130e0490bcc517ef14a554641670845
José Ithalo <jose_ithalo@hotmail.com>
2023-04-20 10:58:53 -0300

    Alteração nas constantes do formulário

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const inputName = document.querySelector('#name');
-const inputNumber = document.querySelector('#number');
+const pokeForm = document.querySelector('form');
+const inputValue = document.querySelector('#search');
+// const inputName = document.querySelector('#name');
+// const inputNumber = document.querySelector('#number');
 const titleName = document.querySelector('h2');
 const valueType = document.querySelector('.valueType');
 const pokePhoto = document.querySelector('.front');
@@ -10,53 +12,55 @@ const informations = document.querySelector('.informBallon');
 const pokemonImages = document.querySelector('.pokemonImages');
 const reverseButton = document.querySelector('.reverseButton');
 
-const inputList = [inputName, inputNumber];
+// const inputList = [inputName, inputNumber];
 
-inputList.forEach(function (input) {
+// inputList.forEach(function (input) {
 
-    input.addEventListener('change', function () {
+pokeForm.addEventListener('submit', function (evt) {
 
-        const url = 'https://pokeapi.co/api/v2/pokemon/'
+    evt.preventDefault();
 
-        const promiseResposta = fetch(url + input.value.toLowerCase());
-        promiseResposta.then(function (resposta) {
+    const url = 'https://pokeapi.co/api/v2/pokemon/'
 
-            if (!resposta.ok) {
-                errorBallon.classList.remove('hidden');
-                informations.classList.add('hidden');
-                pokemonImages.classList.add('hidden');
-                return;
-            } else {
-                informations.classList.remove('hidden');
-                pokemonImages.classList.remove('hidden');
-                errorBallon.classList.add('hidden');
-            }
+    const promiseResposta = fetch(url + inputValue.value.toLowerCase());
+    promiseResposta.then(function (resposta) {
 
-            const promiseBody = resposta.json();
+        if (!resposta.ok) {
+            errorBallon.classList.remove('hidden');
+            informations.classList.add('hidden');
+            pokemonImages.classList.add('hidden');
+            return;
+        } else {
+            informations.classList.remove('hidden');
+            pokemonImages.classList.remove('hidden');
+            errorBallon.classList.add('hidden');
+        }
 
-            promiseBody.then(function (body) {
-                console.log(body);
-                titleName.textContent = body.name;
-                valueType.textContent = body.types[0].type.name;
-                pokePhoto.src = body['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-                pokePhoto2.src = body['sprites']['versions']['generation-v']['black-white']['animated']['back_default'];
-                inputName.value = body.name;
-                inputNumber.value = body.id;
-                abilities.forEach(function (abil) {
-                    abil.textContent = '';
-                });
-                const lengthAbility = body.abilities.length;
-                for (let pos = 0; pos < lengthAbility; pos++) {
-                    abilities[pos].textContent = body.abilities[pos].ability.name;
-                }
+        const promiseBody = resposta.json();
+
+        promiseBody.then(function (body) {
+            console.log(body);
+            titleName.textContent = body.name;
+            valueType.textContent = body.types[0].type.name;
+            pokePhoto.src = body['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+            pokePhoto2.src = body['sprites']['versions']['generation-v']['black-white']['animated']['back_default'];
+            inputValue.value = body.name;
+            // inputNumber.value = body.id;
+            abilities.forEach(function (abil) {
+                abil.textContent = '';
             });
+            const lengthAbility = body.abilities.length;
+            for (let pos = 0; pos < lengthAbility; pos++) {
+                abilities[pos].textContent = body.abilities[pos].ability.name;
+            }
         });
-
     });
-});
 
-reverseButton.addEventListener('click', function () {
-
-    pokePhoto.classList.toggle('hidden');
-    pokePhoto2.classList.toggle('hidden');
 });
+// });
+
+// reverseButton.addEventListener('click', function () {
+
+//     pokePhoto.classList.toggle('hidden');
+//     pokePhoto2.classList.toggle('hidden');
+// });
