e645d105db0b91ef942db792fdd6815fbd633bce
José Ithalo <jose_ithalo@hotmail.com>
2023-04-20 22:29:23 -0300

    Adição de novas variáveis e eventos

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const pokeForm = document.querySelector('form');
 const inputValue = document.querySelector('#search');
-// const inputName = document.querySelector('#name');
-// const inputNumber = document.querySelector('#number');
 const titleName = document.querySelector('h2');
 const valueType = document.querySelector('.valueType');
 const pokePhoto = document.querySelector('.front');
@@ -11,56 +9,83 @@ const errorBallon = document.querySelector('.errorBallon');
 const informations = document.querySelector('.informBallon');
 const pokemonImages = document.querySelector('.pokemonImages');
 const reverseButton = document.querySelector('.reverseButton');
+const buttonPrev = document.querySelector('.btnPrev');
+const buttonNext = document.querySelector('.btnNext');
 
-// const inputList = [inputName, inputNumber];
+const url = 'https://pokeapi.co/api/v2/pokemon/';
+let pokeNumber = 0;
 
-// inputList.forEach(function (input) {
+function renderPokemon(response) {
+
+    if (!response.ok) {
+        errorBallon.classList.remove('hidden');
+        informations.classList.add('hidden');
+        pokemonImages.classList.add('hidden');
+        return;
+    } else {
+        informations.classList.remove('hidden');
+        pokemonImages.classList.remove('hidden');
+        errorBallon.classList.add('hidden');
+    }
+
+    const promiseBody = response.json();
+
+    promiseBody.then(function (body) {
+        console.log(body);
+        titleName.textContent = body.name;
+        valueType.textContent = body.types[0].type.name;
+        pokePhoto.src = body['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        pokePhoto2.src = body['sprites']['versions']['generation-v']['black-white']['animated']['back_default'];
+        inputValue.value = body.name;
+        pokeNumber = body.id;
+        // inputNumber.value = body.id;
+        abilities.forEach(function (abil) {
+            abil.textContent = '';
+        });
+        const lengthAbility = body.abilities.length;
+        for (let pos = 0; pos < lengthAbility; pos++) {
+            abilities[pos].textContent = body.abilities[pos].ability.name;
+        }
+    });
+}
 
 pokeForm.addEventListener('submit', function (evt) {
 
     evt.preventDefault();
 
-    const url = 'https://pokeapi.co/api/v2/pokemon/'
-
-    const promiseResposta = fetch(url + inputValue.value.toLowerCase());
-    promiseResposta.then(function (resposta) {
-
-        if (!resposta.ok) {
-            errorBallon.classList.remove('hidden');
-            informations.classList.add('hidden');
-            pokemonImages.classList.add('hidden');
-            return;
-        } else {
-            informations.classList.remove('hidden');
-            pokemonImages.classList.remove('hidden');
-            errorBallon.classList.add('hidden');
-        }
-
-        const promiseBody = resposta.json();
-
-        promiseBody.then(function (body) {
-            console.log(body);
-            titleName.textContent = body.name;
-            valueType.textContent = body.types[0].type.name;
-            pokePhoto.src = body['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-            pokePhoto2.src = body['sprites']['versions']['generation-v']['black-white']['animated']['back_default'];
-            inputValue.value = body.name;
-            // inputNumber.value = body.id;
-            abilities.forEach(function (abil) {
-                abil.textContent = '';
-            });
-            const lengthAbility = body.abilities.length;
-            for (let pos = 0; pos < lengthAbility; pos++) {
-                abilities[pos].textContent = body.abilities[pos].ability.name;
-            }
-        });
+    const promiseResponse = fetch(url + inputValue.value.toLowerCase());
+    promiseResponse.then(function (response) {
+        renderPokemon(response);
     });
 
 });
-// });
 
-// reverseButton.addEventListener('click', function () {
+buttonPrev.addEventListener('click', function () {
 
-//     pokePhoto.classList.toggle('hidden');
-//     pokePhoto2.classList.toggle('hidden');
-// });
+    if (pokeNumber <= 1) {
+        pokeNumber = 0;
+        return
+    }
+
+    pokeNumber -= 1;
+
+    const promiseResponse = fetch(url + pokeNumber);
+    promiseResponse.then(function (response) {
+        renderPokemon(response);
+    });
+});
+
+buttonNext.addEventListener('click', function () {
+
+    pokeNumber += 1;
+
+    const promiseResponse = fetch(url + pokeNumber);
+    promiseResponse.then(function (response) {
+        renderPokemon(response);
+    });
+});
+
+reverseButton.addEventListener('click', function () {
+    pokePhoto.classList.toggle('hidden');
+    pokePhoto2.classList.toggle('hidden');
+});
